264575e82f6312f891298c1ea63b44c95c706435
Reviewone-ui <bentestore@hotmail.com>
2026-07-08 15:42:58 -0700

    Replace Vite React plugin and remove server config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import reactOxc from '@vitejs/plugin-react-oxc'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-  },
+  plugins: [reactOxc()],
 })
